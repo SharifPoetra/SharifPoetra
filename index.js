b2271fc9985ce7cd87bebb8693623c5c69a33f50
SharifPoetra <sharifpoetra1@gmail.com>
2020-07-23 17:18:23 +0800

    Yosh

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const Mustache = require("mustache");
 const fs = require("fs");
+const puppeteerService = require("./services/puppeteer.service");
+
 const MUSTACHE_MAIN_DIR = "./main.mustache";
 
-/**
- * DATA is the object that contians all
- * the data to be provided to Mustache
- * Notice the "name" and "date" property
- */
 let DATA = {
-  name: "sharif",
-  date: new Date().toLocaleDateString("en-GB", {
+  name: "Sharif",
+  fullname: "SharifPoetra",
+  refresh_date: new Date().toLocaleDateString("en-GB", {
     weekday: "long",
     month: "long",
     day: "numeric",
@@ -17,20 +15,29 @@ let DATA = {
     minute: "numeric",
     timeZoneName: "short",
     timeZone: "Asia/Kuala_Lumpur",
-  })
+  }),
 };
 
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
- function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
+async function setInstagramPosts() {
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount("sharifpoetra", 3);
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
 }
 
-generateReadMe();
+ function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+     if (err)
+       throw err;
+     const output = Mustache.render(data.toString(), DATA);
+     fs.writeFileSync("README.md", output);
+   });
+}
+
+async function action() {
+  await setInstagramPosts();
+  generateReadMe();
+  await puppeteerService.close();
+}
+
+action();
